f3c560e4e18388fdce8d23327a143c94dcd57c2d
Shashi Kiran <kiranshashiny@gmail.com>
2019-09-19 15:54:28 +0530

    Updated

--- opencv-face-recognition/build_dataset.sh
+++ opencv-face-recognition/build_dataset.sh
@@ -1,1 +1,26 @@
-python3 build_face_dataset.py --cascade haarcascade_frontalface_default.xml  --output dataset/sunitha
+#!/bin/bash
+
+if [ "$#" -eq 0 ]
+then
+	
+	echo "" 
+	echo "	Error : Please enter the folder to output the images"
+	echo "	Edit this file e.g;  --dataset/shashi"
+	echo "	Exiting ! "
+	echo ""
+	exit
+fi
+if [ $# == 1 ]
+then
+	echo $1
+	if [ ! -d $1 ]
+	then
+		echo "Directory $1 does not exist"
+		echo "Exiting"
+	else
+		#echo "Directory exists - using it to store new images."
+		python3 build_face_dataset.py --cascade haarcascade_frontalface_default.xml  --output $1
+	fi
+
+fi
+
